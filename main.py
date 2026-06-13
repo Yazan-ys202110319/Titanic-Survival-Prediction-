bf83fedd55522fb67db1b4d3802f8d902200cea1
Yazan Jumaa <ys2110319@qu.edu.qa>
2026-06-13 15:15:36 +0300

    Data cleaning and preprocessing

--- main.py
+++ main.py
@@ -4,3 +4,26 @@ import string
 import seaborn as sns
 import matplotlib.pyplot as plt
 
+data = pd.read_csv('Titanic-Dataset.csv')
+
+print(data.head())
+
+
+
+# Data cleaning 
+
+print(data.isnull().sum())
+print(data.info())
+
+print(data["Name"])
+
+
+data["Age"] = data["Age"].fillna(data["Age"].mean())
+print(data.tail())
+
+
+data = data.drop(columns=["Cabin"])
+print(data)
+
+
+# feature engineering 
